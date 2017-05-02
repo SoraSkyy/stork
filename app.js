94d833a58614f6e008125f0f9c65cf07ac1a7d87
SoraSkyy <viorama@gmail.com>
2017-05-03 01:06:06 +0800

    ESlinted the code

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var expressSession = require('express-session');
-var MongoStore = require('connect-mongo')(expressSession);
-var mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const expressSession = require('express-session');
+const MongoStore = require('connect-mongo')(expressSession);
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/storkapp');
 
 // Passport-related initialization.
-var User = require('./models/user');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  },
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+const User = require('./models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var login = require('./routes/login');
-var signup = require('./routes/signup');
+passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, (username, password, done) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  });
+}));
 
-var app = express();
+const index = require('./routes/index');
+const users = require('./routes/users');
+const login = require('./routes/login');
+const signup = require('./routes/signup');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -53,14 +49,14 @@ app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSession({
-		secret: 'test', // TODO: Store secret outside of source code.
-		store: new MongoStore({
-        	host: '127.0.0.1',
-        	port: '27017',
-        	url: 'mongodb://localhost:27017/storkapp'
-    		}),
-		saveUninitialized: false,
-		resave: false}));
+  secret: 'test', // TODO: Store secret outside of source code.
+  store: new MongoStore({
+    host: '127.0.0.1',
+    port: '27017',
+    url: 'mongodb://localhost:27017/storkapp',
+  }),
+  saveUninitialized: false,
+  resave: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -70,14 +66,14 @@ app.use('/login', login);
 app.use('/signup', signup);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
