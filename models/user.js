cd6dd445e2150e1948a49d0d3dc98a101fd9883e
SoraSkyy <viorama@gmail.com>
2017-05-03 01:29:13 +0800

    Linted user.js and added bcrypt-nodejs dependency for salting and hashes

--- models/user.js
+++ models/user.js
@@ -1,26 +1,20 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // TODO: Decide the structure of the User model
-var UserSchema = Schema(
+const UserSchema = Schema(
   {
-    email: {type: String, required: true, max: 100},
-    password: {type: String, required: true, max: 100},
-    date_of_birth: {type: Date},
-    profile_id: {type: String, max:100},
-    profile_name: {type: String, required: false, max: 100}
-  }
+    email: { type: String, required: true, max: 100 },
+    password: { type: String, required: true, max: 100 },
+    date_of_birth: { type: Date },
+    profile_id: { type: String, max: 100 },
+    profile_name: { type: String, required: false, max: 100 },
+  },
 );
 
-
-
 // Virtual for author's URL to obtain the particular instance
-UserSchema
-.virtual('url')
-.get(function () {
-  return '/profile/' + this._id;
-});
+UserSchema.virtual('url').get(() => `/profile/ ${this.profile_id}`);
 
-//Export model
+// Export model
 module.exports = mongoose.model('User', UserSchema);
