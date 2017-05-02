54f03c668028bfcd52e247ea0c89a2df812cbe30
SoraSkyy <viorama@gmail.com>
2017-05-03 01:53:46 +0800

    Fixed syntax bug.

--- models/user.js
+++ models/user.js
@@ -4,14 +4,13 @@ const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 // TODO: Decide the structure of the User model
-const UserSchema = Schema(
-  {
+const UserSchema = Schema({
     email: { type: String, required: true, max: 100 },
     password: { type: String, required: true, max: 100 },
     date_of_birth: { type: Date },
     profile_id: { type: String, max: 100 },
     profile_name: { type: String, required: false, max: 100 },
-  },
+  }
 );
 
 // Virtual for author's URL to obtain the particular instance
