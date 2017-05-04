bc532e9543dba0829010a85b04cffea2883782f1
SoraSkyy <viorama@gmail.com>
2017-05-05 00:46:46 +0800

    Remove commented code.

--- routes/signup.js
+++ routes/signup.js
@@ -21,28 +21,6 @@ router.get('/', function(req, res, next) {
   }
 });
 
-/*
-router.post('/', (req, res) => {
-  req.checkBody('email', 'Invalid email.').isEmail();
-  req.checkBody('profile_name', 'Profile name must be between 4 to 20 characters.').len(4,20);
-  req.checkBody('profile_name', 'Profile name can only contain letters and numbers.').isAlphanumeric();
-  req.checkBody('password', 'Password length must be between 6 to 20 characters.').len(6,20);
-
-  req.getValidationResult().then((result) => {
-    result.isEmpty() ? (passport.authenticate('local-signup', {
-  successRedirect: '/login',
-  failureRedirect: '/signup',
-  failureFlash: true,
-  }))(req, res) : res.render('signup', {
-    'profile_name':req.body.profile_name,
-    'password':req.body.password,
-    'email':req.body.email,
-    message: req.flash('signupMessage')});
-  });
-
-});
-*/
-
 router.post('/', (req, res) => {
   req.session.signupEmail = req.body.email;
   req.session.signupProfileName = req.body.profile_name;
